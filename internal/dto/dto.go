d9c648d5c1e960f2fe85a8404a58e18e2ab86a94
Dmitry Dyakov <dyakov270@gmail.com>
2026-07-21 09:56:52 +0300

    iter 23: fix staticlint issues

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -1,6 +1,8 @@
 // Package dto содержит объекты передачи данных между компонентами системы.
 package dto
 
+// Metric представляет собой DTO метрики
+//
 // generate:reset
 type Metric struct {
 	ID    string   `json:"id"`
